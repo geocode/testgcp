3f50a3b084f673782460bf0fba192ecef1f2f7d8
Geoff Ruscoe <Geoff_Ruscoe@papajohns.com>
2017-11-05 10:04:21 -0500

    Pushing a new version to prod using spinnaker

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello GCP CD!")
+	io.WriteString(w, "Hello! Check it out -- a new update to GCP CD!")
 }
 
 func main() {
